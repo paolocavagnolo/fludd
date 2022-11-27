b6c7255ea355e91473630ab116fb01e83e045696
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2022-11-27 10:50:52 +0100

    Update maturazione.ino

--- select/maturazione.ino
+++ select/maturazione.ino
@@ -1,7 +1,8 @@
 #include <FastLED.h>
 
-#define NUM_LEDS 64 
-#define COM_PIN 10
+#define NUM_LEDS 64
+#define COM_PIN 6
+#define BTN_IN 7
 
 CRGB leds[NUM_LEDS];
 
@@ -9,16 +10,17 @@ void setup() {
 
   FastLED.addLeds<WS2812B, COM_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(255);
-  
+
   FastLED.clear();
   FastLED.show();
 
-  pinMode(11,INPUT_PULLUP);
+  pinMode(BTN_IN, INPUT);
   Serial.begin(9600);
 
 }
 
 uint8_t j = 0;
+uint8_t je = 32;
 uint8_t i = 0;
 uint8_t w1 = 0;
 uint8_t w2 = 0;
@@ -31,35 +33,38 @@ unsigned long startT = 0;
 
 void loop() {
 
-  if (!digitalRead(11)) {
+  if (!digitalRead(BTN_IN)) {
     beam = true;
     startT = millis();
   }
 
   if (beam) {
-    if ((millis() - startT) > 7000) {
-      w1 = random(245,255);
-      w2 = random(0,10);
-      w3 = random(0,1);
-      
+    if ((millis() - startT) > 7200) {
+      w1 = random(245, 255);
+      w2 = random(0, 2);
+      w3 = random(0, 1);
+
       if (w3) {
         w = w1;
       }
       else {
         w = w2;
       }
-      
-      for (i = 0; i < 254; i = i + 2) {
-        fill_solid(leds,NUM_LEDS,CHSV(w,255,i));
-        FastLED.show();
-        delay(5);
+
+      for (int kk = 0; kk < 3; kk++) {
+        for (i = 0; i < 254; i = i + 2) {
+          fill_solid(leds, NUM_LEDS, CHSV(w, 255, i));
+          FastLED.show();
+          delay(10);
+        }
+        for (i = 254; i > 0; i = i - 2) {
+          fill_solid(leds, NUM_LEDS, CHSV(w, 255, i));
+          FastLED.show();
+          delay(10);
+        }
       }
-      for (i = 254; i > 0; i = i - 2) {
-        fill_solid(leds,NUM_LEDS,CHSV(w,255,i));
-        FastLED.show();
-        delay(1);
-      }
-      beam = false; 
+     
+      beam = false;
     }
     else {
       for (i = 0; i < NUM_LEDS; i++) {
@@ -73,24 +78,21 @@ void loop() {
     }
   }
   else {
-    
-    for (i = 0; i < 254; i = i + 2) {
-      leds[j] = CHSV(w,255,i);
+
+    for (i = 0; i < 20; i++) {
+      leds[j] = CHSV(224, 255, i);
+      leds[je] = CHSV(224, 255, 19 - i);
       FastLED.show();
     }
-  
-    leds[j] = CHSV(w,255,255);
-    FastLED.show();
-  
-    for (i = 0; i < NUM_LEDS; i++) {
-      leds[i].nscale8(220);
-      FastLED.show();
-    }
-  
+    delay(30);
     j = j + 1;
-  
+    je = je + 1;
+
     if (j >= NUM_LEDS) {
       j = 0;
+    }
+    if (je >= NUM_LEDS) {
+      je = 0;
     }
   }
 }
