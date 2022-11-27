2a40c70cfdc5f75acf9226fafbe8359b270de509
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2022-11-27 10:51:20 +0100

    Update tuboLed.ino

--- select/tuboLed.ino
+++ select/tuboLed.ino
@@ -1,7 +1,8 @@
 #include <FastLED.h>
 
 #define NUM_LEDS 240
-#define COM_PIN 9
+#define COM_PIN 5
+#define BTN_IN 7
 
 #define SALITA 70
 #define ORIZZ 180
@@ -11,17 +12,19 @@ CRGB leds[NUM_LEDS];
 
 void setup() {
 
-  pinMode(8,INPUT_PULLUP);
-  
+  pinMode(BTN_IN, INPUT_PULLUP);
+
   FastLED.addLeds<WS2812B, COM_PIN, GRB>(leds, NUM_LEDS);
   FastLED.setBrightness(255);
-  
-  FastLED.clear();
-  
 
-  fill_solid(leds,NUM_LEDS,CHSV(25,255,150));
+  FastLED.clear();
+
+
+  fill_solid(leds, NUM_LEDS, CHSV(25, 255, 150));
   FastLED.show();
-  
+
+  Serial.begin(9600);
+
 
 }
 
@@ -34,11 +37,13 @@ bool beam = false;
 unsigned long startT = 0;
 unsigned long fadeT = 0;
 
-bool sali=true;
+bool sali = true;
 
 void loop() {
 
-  if (!digitalRead(8)) {
+  Serial.println(digitalRead(BTN_IN));
+
+  if (!digitalRead(BTN_IN)) {
     beam = true;
     startT = millis();
   }
@@ -46,8 +51,15 @@ void loop() {
   if (beam) {
     if ((millis() - startT) > 3) {
       for (i = 0; i < NUM_LEDS; i++) {
-        leds[i] = CHSV(1,255,255);
+        leds[i] = CHSV(map(i, 0, NUM_LEDS, 5, -6), 255, 255);
         delay(25);
+        FastLED.show();
+      }
+      delay(2500);
+      for (int jj = 255; jj >= 0; jj--) {
+        for (i = 0; i < NUM_LEDS; i++) {
+          leds[i] = CHSV(map(i, 0, NUM_LEDS, 5, -6), 255, jj);
+        }
         FastLED.show();
       }
       beam = false;
@@ -55,10 +67,10 @@ void loop() {
   }
   else {
 
-    fill_solid(leds,NUM_LEDS,CHSV(25,255,j));
+    fill_solid(leds, NUM_LEDS, CHSV(-6, 255, j));
     FastLED.show();
 
-    if ((millis() - fadeT) > 30) {
+    if ((millis() - fadeT) > 10) {
       fadeT = millis();
       if (sali) {
         j++;
@@ -66,9 +78,9 @@ void loop() {
       else {
         j--;
       }
-      if (j>150 || j<1) {
+      if (j > 100 || j < 1) {
         sali = !sali;
       }
-    } 
+    }
   }
 }
