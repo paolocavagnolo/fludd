294511ee00d0193d4cb942dfc7290a3576ff51b8
Paolo Cavagnolo <paolo.cavagnolo@gmail.com>
2024-05-07 14:37:57 +0200

    Update BLENDING_CC.ino

--- select/consegna/BLENDING_CC.ino
+++ select/consegna/BLENDING_CC.ino
@@ -1,22 +1,30 @@
-#define goPin 6
-#define rel_1 10
-#define rel_2 11
-#define rel_3 12
+// Arduino UNO
+// coded 7/5/2024 by Paolo C.
 
-#define TRAVEL_LEFT 2000
-#define PAUSE_LEFT 500
-#define TRAVEL_RIGHT 2000
-#define PAUSE_RIGHT 500
+#define goPin 9
+#define rel_1 10 //+12 V
+#define rel_2 11 // switch cable A
+#define rel_3 12 // switch cable B
+
+#define TRAVEL_LEFT 3500
+#define PAUSE_LEFT 1500
+#define TRAVEL_RIGHT 3500
+#define PAUSE_RIGHT 1500
 
 void setup() {
-  delay(1000);
+
+  digitalWrite(rel_1, HIGH);
+  digitalWrite(rel_2, HIGH);
+  digitalWrite(rel_3, HIGH);
+
+  delay(50);
 
   pinMode(goPin, INPUT);
   pinMode(rel_1, OUTPUT);
   pinMode(rel_2, OUTPUT);
   pinMode(rel_3, OUTPUT);
 
-  delay(1000);
+  delay(50);
 }
 
 bool goS = false;
@@ -46,20 +54,51 @@ void loop() {
     }
   }
 
-
   // act
   if (goStart) {
     goStart = false;
 
     goLeft(TRAVEL_LEFT / 2);
+    stop(PAUSE_LEFT);
 
     for (int i = 0; i < 4; i++) {
-      goLeft(TRAVEL_RIGHT);
-      delay(PAUSE_RIGHT);
-      goRight(TRAVEL_LEFT);
-      delay(PAUSE_LEFT);
+      goRight(TRAVEL_RIGHT);
+      stop(PAUSE_RIGHT);
+      goLeft(TRAVEL_LEFT);
+      stop(PAUSE_LEFT);
     }
 
     goRight(TRAVEL_RIGHT / 2);
+  } else {
+    idle();
   }
+
 }
+
+void idle() {
+  digitalWrite(rel_3, HIGH);
+  digitalWrite(rel_2, HIGH);
+  digitalWrite(rel_1, HIGH);
+}
+
+void stop(int tt) {
+  digitalWrite(rel_1, HIGH);
+  delay(tt);
+}
+
+void goLeft(int tt) {
+  digitalWrite(rel_3, HIGH);
+  digitalWrite(rel_2, HIGH);
+  delay(100);
+  digitalWrite(rel_1, LOW);
+  delay(tt-100);
+}
+
+void goRight(float tt) {
+  digitalWrite(rel_3, LOW);
+  digitalWrite(rel_2, LOW);
+  delay(100);
+  digitalWrite(rel_1, LOW);
+  delay(tt-100);
+}
+
